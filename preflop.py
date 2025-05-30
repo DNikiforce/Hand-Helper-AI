8e5947fd2a240ab50cd441487c916cb1f9f77875
DNikiforce <dimasn567@gmail.com>
2025-05-30 01:48:02 -0600

    Delete preflop.py

--- preflop.py
+++
@@ -1,8 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def handle_preflop(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
-    await update.message.reply_text(f"📥 Получена текстовая раздача:\n{text}")
-
-
