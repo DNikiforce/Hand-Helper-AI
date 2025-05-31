4adcfacca289df6b8d4fb107d7be38e254e72607
DNikiforce <dimasn567@gmail.com>
2025-05-30 18:10:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import os
 from context.session import GameSessionManager
 # Создаём глобальный менеджер сессий пользователей
 session_manager = GameSessionManager()
+from handlers.preflop import handle_preflop
+from handlers.flop import handle_flop
+from handlers.turn import handle_turn
+from handlers.river import handle_river
+from handlers.result import handle_result
 from logic.equity import calculate_equity_and_outs
 from utils.parser import parse_cards_from_emojis
 from fastapi import FastAPI, Request
@@ -33,6 +38,12 @@ async def on_startup():
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(MessageHandler(filters.VOICE, handle_voice))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_preflop))
+    telegram_app.add_handler(MessageHandler(filters.Regex(r"^PF:"), handle_preflop))
+telegram_app.add_handler(MessageHandler(filters.Regex(r"^FLO:"), handle_flop))
+telegram_app.add_handler(MessageHandler(filters.Regex(r"^TUR:"), handle_turn))
+telegram_app.add_handler(MessageHandler(filters.Regex(r"^RIV:"), handle_river))
+telegram_app.add_handler(MessageHandler(filters.Regex(r"^RES:"), handle_result))
+
     await application.initialize()
     await application.start()
     await application.bot.set_webhook(WEBHOOK_URL)
